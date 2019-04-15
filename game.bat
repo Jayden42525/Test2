18659757f26dc721de8bdec566198f70ef257d96
Jayden42525 <48032509+Jayden42525@users.noreply.github.com>
2019-04-15 14:57:52 -0700

    Update game.bat

--- game.bat
+++ game.bat
@@ -1,33 +1,62 @@
 @echo off
 cls
-:start
-echo.
-echo 1) Print Hello
-echo 2) Print Bye
-echo 3) Print Test
+
+:startScreen
+echo   ___________      _______        _______       _____    _____         _________________    ____________
+echo  /            \   |       |      |       |     /\ | /\  /\ | /\       |                 |  /            \ 
+echo |             |   |       |      |       |    | -[X]- || -[X]- |      |                 |  |             | 
+echo  \        ___/    |       |      |       |   __\/_|_\/  \/_|_\/____   |_____      ______|   \        ___/
+echo   \        \      |       |______|       |  /\ | /\          /\ | /\       |      |          \       \
+echo     \       \     |                      | | -[X]- |        | -[X]- |      |      |           \       \ 
+echo      \       \    |                      |  \/_|_\/__    ____\/_|_\/       |      |            \       \  
+echo   ____\       \   |        _______       |     /\ | /\  /\ | /\            |      |         ____\       \ 
+echo  /             \  |       |       |      |    | -[X]- || -[X]- |           |      |        /             \ 
+echo  \_____________/  |_______|       |______|     \/_|_\/  \/_|_\/            |______|        \_____________/
+echo 
+echo
+echo
+echo Press the button "s" to start...
+echo Press the button "e" to end all hope...
+pause
+
 set choice=
 set /p choice=Type the number to print text.
 if not '%choice%'=='' set choice=%choice:~0,1%
-if '%choice%'=='1' goto hello
-if '%choice%'=='2' goto bye
-if '%choice%'=='3' goto test
-if '%choice%'=='[4,5,6,7,8,9,0]' {
-	then{
-		echo %choice%" is not valid, try again
-		goto choice}
-		  }
-echo "%choice%" is not valid, try again
-echo.
-goto start
-:hello
-echo HELLO
-goto end
-:bye
-echo BYE
-goto end
-:test
-echo TEST
-goto end
-:end
+if '%choice%'=='s' goto warning
+if '%choice%'=='e' goto Exit
+
+:warning
+cls
+echo !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!
+echo 
+echo
+echo Welcome to Shots... The purpose of this game is to spread awareness of chronic depression and raise awareness
+echo to the raising numbers of people commiting suicide. This game is not for the light-hearted. If you are one of
+echo these people, please turn around and find another game. This game is dark, and extremely morbid. Please go on
+echo       at your own risk. Whatever choices you make are not mine, and I hold no liability for your actions. 
+echo 
+echo                                         Press anything to continue
+echo 
+echo !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!
+pause
+goto Start
+
+:Start
+
+
+echo      _____    _____
+echo     /\ | /\  /\ | /\
+echo    | -[X]- || -[X]- |
+echo   __\/_|_\/  \/_|_\/____
+echo  /\ | /\          /\ | /\
+echo | -[X]- |        | -[X]- |
+echo  \/_|_\/__    ____\/_|_\/
+echo     /\ | /\  /\ | /\
+echo    | -[X]- || -[X]- |
+echo     \/_|_\/  \/_|_\/   
+
+
+:Exit
+echo you failed us.....
 pause
 exit
