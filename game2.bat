0e690927e3e43d310789d23e8e1792420c0fb7cf
Jayden42525 <48032509+Jayden42525@users.noreply.github.com>
2019-04-24 13:28:28 -0700

    Update game2.bat

--- game2.bat
+++ game2.bat
@@ -2,13 +2,13 @@
 cls
 
 :startScreen
-echo   ___          ___         __         ___      ___    _____________     __________
-echo  |   \        /   |       /  \       |   \    |   |  |             |   |          |
+echo   ___          ___                    ___      ___                      __________
+echo  |   \        /   |       ____       |   \    |   |   _____________    |          |
 echo  |     \    /     |      /    \      |     \  |   |  |____     ____|   |      ____|
 echo  |       \/       |     /  /\  \     |       \|   |       |   |        |    |
 echo  |   |\      /|   |    /  /  \  \    |   |\       |   ____|   |____    |    |_____
-echo  |   |  \  /  |   |   /  /    \  \   |   |  \     |  |             |   |          |
-echo  |___|        |___|  /__/      \__\  |___|    \___|  |_____________|   |__________|
+echo  |   |  \  /  |   |   /__/    \__\   |   |  \     |  |_____________|   |          |
+echo  |___|        |___|                  |___|    \___|                    |__________|
 echo
 echo Press the button "s" to start...
 echo Press the button "e" to end all hope...
@@ -18,24 +18,9 @@ pause
 set choice=
 set /p choice=Type the number to print text.
 if not '%choice%'=='' set choice=%choice:~0,1%
-if '%choice%'=='s' goto warning
+if '%choice%'=='s' goto Start
 if '%choice%'=='e' goto Exit
 if if '%choice%'=='f' goto respec
-:warning
-cls
-echo !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!
-echo 
-echo
-echo Welcome to Shots... The purpose of this game is to spread awareness of chronic depression and raise awareness
-echo to the raising numbers of people commiting suicide. This game is not for the light-hearted. If you are one of
-echo these people, please turn around and find another game. This game is dark, and extremely morbid. Please go on
-echo       at your own risk. Whatever choices you make are not mine, and I hold no liability for your actions. 
-echo 
-echo                                         Press anything to continue
-echo 
-echo !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!   !!!WARNING!!!
-pause
-goto Start
 
 :Start
 cls
@@ -45,7 +30,7 @@ echo (You wake up to the sound of your phone going off.) "Beautiful day isn't it
 echo (Your phone goes off again but the number is an unknown and foreign looking number.) "What'd you think Jack?" 
 echo (Jack just whimpers wanting to go outside.) 
 echo
-echo 1) *Answer phone*
+echo 1) *Answer phone* (DON'T CHOOSE, NOT FINISHED)
 echo 2) (Don't answer) Let's get you fed, boy.
 
 set choice=
@@ -71,9 +56,6 @@ if '%choice%'=='1' goto callCont
 if '%choice%'=='2' goto truth1
 if '%choice%'=='3' goto end1
 
-:callCont
-cls
-echo 
 
 :feedJack
 cls
@@ -83,24 +65,71 @@ echo
 echo 1) *You decide to take Jack with you so you grab his leash* *Jack gets excited* “Come on buddy”
 echo 2) *You lock up Jack in his cage and head out the door* “I’ll see you when I get back I’m just 
 echo                            going to run out and get you some food”
-
+echo
 set choice=
 set /p choice=Type the number to print text.
 if not '%choice%'=='' set choice=%choice:~0,1%
 if '%choice%'=='1' goto grabLeash
-if '%choice%'=='2' goto
+if '%choice%'=='2' goto radio
+
+:radio
+cls
+echo *You walk out to your dull blue sedan, get in the car, and drive off to the store* *You turn on
+echo the radio to fill the empty air* *you turn it to a random radio station and continue driving to
+echo the store*
+echo
+echo (Push A button to continue)
+pause 
+echo *While driving you see something, something that takes you back, it grabs your focus, and you 
+echo can’t stop thinking about it* *By the time you escape your own thoughts and remember that you’re
+echo driving it’s too late, you’re about to run a red light and see cars coming yet you speed up*
+echo *Everything goes black* *The last thing you remember is plenty of people shouting around you*
+echo
+echo                             “GET HIM OUT, GET HIM OUT!”    
+echo
+echo
+echo OOF! YOU WON BAD ENDING ONE...
+echo
+echo (Push a button to continue)
+pause
+goto startScreen
 
 :grabLeash
 cls
-echo 
+echo *You walk out to your dull blue sedan with Jack’s leash in your hand* *you open the car door* 
+echo “Come on Jack, get in buddy” *Jack gets in the backseat and settles in while you get into the 
+echo driver seat and drive off to the store* 
 echo
+echo 1) "Let's go boy"
+echo 2) "You know what, let's walk to the store!"
+set choice=
+set /p choice=Type the number to print text.
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='1' goto store
+if '%choice%'=='2' goto walk (NOT FINISHED, DON'T CHOOSE)
 
-
-goto startScreen
-
-:Exit
+:store
 cls
-echo
-echo you failed us.....
+echo *You and Jack are greeted by friendly workers* *You walk to the dog food section, passing many
+echo colorful aisles of toys, clothes, and necessities for other pets when finally you get to the 
+echo dog food* *you look through the array of food and find the one Jack likes* *You pick it up and
+echo carry it to the cash register, while you pay for the food you receive compliments from stranger
+echo about how cute Jack is* *You smile and say* “Thank you”
+echo 
+echo (Push A button to continue)
 pause
-exit
+cls 
+echo *You receive your receipt and you leave the store* *You load Jack into the backseat once again and 
+echo you get in the driver side* *You arrive at your house, grab Jack, and immediately go inside to feed
+echo Jack* *Jack barks in what seems to be a contented way as he began eating*
+echo (Push A button to continue)
+pause 
+echo *You smile at Jack as he eats* *You think about how lucky you are to have him and how different your
+echo life would be without him*    
+echo
+echo
+echo CONGRATS YOU WON GOOD ENDING ONE!!!
+echo
+echo (Push A button to continue)
+pause
+goto startScreen
